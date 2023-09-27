62613e3a042b06c5ba32dba0b1a2f7e2bdc0e599
Pour1a-D <pour1a.dra80@gmail.com>
2023-09-27 20:04:45 +0330

    session03 homework

--- python-tutorial/Session03/homework.py
+++ python-tutorial/Session03/homework.py
@@ -1,13 +1,46 @@
-# 1. Write a Python program to sum all the items in a list.
+my_list: list = [1, 5, -15, 55, 63, 239]
+counter = 0
+len = len(my_list)
+
+# p1. Write a Python program to sum all the items in a list.
+# sum1 = 0
+# while counter < len:
+#     sum1 += my_list[counter]
+#     counter += 1
+
+# print(f"Thw sum is: {sum1}")
 
 
 # 2. Write a Python program to multiply all the items in a list.
+# mul1 = 1
+# while counter < len:
+#     mul1 *= my_list[counter]
+#     counter += 1
 
+# print(f"The mul is: {mul1}")
 
 # 3. Write a Python program to get the largest number from a list.
+# max = -99999
 
+# while counter < len:
+#     if my_list[counter] > max:
+#         max = my_list[counter]
+#     counter += 1
+# print(f"The Maximum number is: {max}")
 
 # 4. Write a Python program to get the smallest number from a list.
+# min = 99999
+
+# while counter < len:
+#     if my_list[counter] < min:
+#         min = my_list[counter]
+#     counter += 1
+
+# print(f"The minimum number is: {min}")
 
 
 # 5. Turn every item of a list into its square
+# while counter < len:
+#     sq = my_list[counter] * my_list[counter]
+#     print(sq)
+#     counter += 1
